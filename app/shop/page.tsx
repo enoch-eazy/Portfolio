cfea90b6f4fb8a639886e829f58bec4337e09a51
Enoch <jesulanaenoch@gmail.com>
2025-08-04 11:34:03 +0100

    refactor: restructure ShopPage component for improved layout and responsiveness

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -5,12 +5,34 @@ import Button from "../ui/button";
 import { sectionPadding } from "../styles/styles";
 import KidsShop from "./getItem/page";
 import GetItem from "./getItem/page";
+import { Icons } from "../ui/icons";
 
 
 export default function ShopPage() {
     return (
-        <main className={`${sectionPadding} mx-auto py-4 mt-20 w-1/2`}>
-           <div className="flex justify-between items-center mx-auto bg-[#0F2B22] p-2">
+        <>
+        {/* Desktop Screen  */}
+        <main className={`hidden md:flex py-4 mt-10 w-full h-screen `}>
+           <div className="flex flex-col px-6 bg-[#0F2B22] w-[250px] min-w-3xs">
+                <Link href={"/"} className="cursor-pointer mt-10">
+                    <Image src={images.logo} alt="Logo" className="size-24 invert object-contain"/>
+                </Link>
+                
+                <Link 
+                className="font-bold text-white mt-4 flex gap-4 items-center ml-4"
+                href={"/shop/addItem"}>
+                    Add Item <Icons.ArrowRight/>
+                </Link>
+               
+           </div>
+           <div className="flex items-center ml-4">
+                <GetItem />
+           </div>
+        </main>
+
+        {/* Mobile Screen  */}
+        <main className={`${sectionPadding} grid md:hidden py-4 mt-20 w-full`}>
+           <div className="flex justify-between items-center w-full bg-[#0F2B22] p-2">
                 <Link href={"/"} className="cursor-pointer">
                     <Image src={images.logo} alt="Logo" className="w-14 h-14 invert"/>
                 </Link>
@@ -24,5 +46,6 @@ export default function ShopPage() {
                 <GetItem />
            </div>
         </main>
+        </>
     )
 }
